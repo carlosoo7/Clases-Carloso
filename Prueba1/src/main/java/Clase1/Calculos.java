460662e95aa705545f564542b357ae07f84c7e2a
carlosoo7 <Carlosfer047@gmail.com>
2026-04-06 17:22:30 -0500

    Inicio

---
+++ Prueba1/src/main/java/Clase1/Calculos.java
@@ -0,0 +1,21 @@
+package Clase1;
+
+public class Calculos {
+
+    private int numero1, numero2;
+
+    public double sumar(double suma1, double suma2){
+        return suma1+suma2;
+    }
+    public double restar(double resta1, double resta2){
+        return resta1-resta2;
+    }
+    public double multiplicar(double multiplicador1, double multiplicador2){
+        return multiplicador1*multiplicador2;
+    }
+    public double dividir(double divisor1, double divisor2){
+        return divisor1/divisor2;
+    }
+
+
+}
---
+++ src/main/java/Clase2/Main.java
@@ -0,0 +1,35 @@
+package Clase2;
+
+
+public class Main {
+    public static void main(String[] args) {
+         //int nota_1=10;
+         //int nota_2=10;
+         //int nota_3=10;
+        int[] notas = new int[5]; //array basico
+        notas[0]=10;
+        notas[1]=20;
+        notas[2]=30;
+
+        int[] notas2 = {10,20,35}; //array de 3 pociciones ya declaradas
+        notas2[0]=13; //cambio de dato
+
+        int[][] notas3 = new int[5][5]; //array estilo matris (0,1), (0,2), (1,1), (1,2)
+        notas3[0][0]=10;
+        notas3[0][1]=20;
+        notas3[0][2]=30;
+        notas3[0][3]=40;
+
+        String[] notas4 = new String[5];
+        notas4[0] = "a";
+
+        for (int i = 0; i < notas4.length; i++) {
+            System.out.println(notas4[i]); //da null las pociciones vacias
+        }
+        for(int i = 0; i < notas3.length; i++) {
+            for(int j = 0; j < notas3.length; j++) {
+                System.out.println(notas3[i][j]); //da 0 las pociciones vacias
+            }
+        }
+    }
+}
