55cdcad5a5adf1b4b7674a1d95f1fb466985bc6e
carlosoo7 <151805986+carlosoo7@users.noreply.github.com>
2026-04-14 19:51:36 -0500

    Update usuario.java

--- src/main/java/Clase3/usuario.java
+++ src/main/java/Clase3/usuario.java
@@ -1,9 +1,10 @@
 package Clase3;
 
 public class usuario extends Persona {
-
-
+    
     //dale un nuevo variable como tipo de usuario y sobreescribe el metodo inforacion
     // para dar toda la info posible (falta telefono y tipo de usuario) y que aparezca en el main
-    
+    // hacerle set y get a tipo de ususario y que en el set se verifique si el usuario es "vip" o "regular"
+    //en caso contrario que se lance una excepcion con mensaje personalizado
+
 }
