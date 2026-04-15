c70d894df83b407f1b2d682b3fbe927e302bd57c
Antaur95 <andrestabares2012@gmail.com>
2026-04-14 19:54:48 -0500

    Usuario

--- src/main/java/Clase3/usuario.java
+++ src/main/java/Clase3/usuario.java
@@ -1,6 +1,11 @@
 package Clase3;
 
 public class usuario extends Persona {
+
+    public usuario(String nombre, String apellido, int edad, String email, String telefono){
+        super(nombre, apellido, edad, email, telefono); // llama al constructor padre
+
+    }
     
     //dale un nuevo variable como tipo de usuario y sobreescribe el metodo inforacion
     // para dar toda la info posible (falta telefono y tipo de usuario) y que aparezca en el main
