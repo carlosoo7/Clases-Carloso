725aa20b0bde0f075b8bce32b76b567cfd698f83
Antaur95 <andrestabares2012@gmail.com>
2026-04-27 17:46:46 -0500

    Tarea

--- src/main/java/Clase3/usuario.java
+++ src/main/java/Clase3/usuario.java
@@ -1,15 +1,26 @@
 package Clase3;
 
 public class usuario extends Persona {
+    private String tipo_de_usuario;
 
-    public usuario(String nombre, String apellido, int edad, String email, String telefono){
+    public usuario(String nombre, String apellido, int edad, String email, String telefono, String tipo_de_usuario) {
         super(nombre, apellido, edad, email, telefono); // llama al constructor padre
+        this.tipo_de_usuario = tipo_de_usuario;
 
     }
-    
+    public String tipo_de_usuario() {
+        return tipo_de_usuario;
+    }
+    public void setTipo_de_usuario(String tipo_de_usuario) { this.tipo_de_usuario = tipo_de_usuario; }
+    }
+    @Override
+    void informacionGeneral(){
+        System.out.println("Nombre: " + super.getNombre() + " Apellido: " + super.getApellido());
+
+    }
     //dale un nuevo variable como tipo de usuario y sobreescribe el metodo inforacion
     // para dar toda la info posible (falta telefono y tipo de usuario) y que aparezca en el main
     // hacerle set y get a tipo de ususario y que en el set se verifique si el usuario es "vip" o "regular"
     //en caso contrario que se lance una excepcion con mensaje personalizado
 
-}
+
